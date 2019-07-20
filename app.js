ac137b33c0608f63140d4d3ee5eea040af328e72
aagadullin <aagadullin@localhost.localdomain>
2019-07-20 22:41:03 +0300

    mysql back result

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({extended:false}));
 
 app.use("/auth", authRouter);
 app.use("/main", mainRouter);
-app.use("/user", userRouter);
+app.use("/users", userRouter);
 
 app.get("/about", function(request, response){
     response.render("main.hbs");
